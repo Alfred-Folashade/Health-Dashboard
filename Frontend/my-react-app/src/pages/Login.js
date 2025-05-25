9b00cd21ca4bd4a1dbc1518cb303d3ffb3df05e6
Alfred-Folashade <129620082+Alfred-Folashade@users.noreply.github.com>
2025-05-25 16:56:54 +0100

    Add handleLogin and handleRegister function

--- Frontend/my-react-app/src/pages/Login.js
+++ Frontend/my-react-app/src/pages/Login.js
@@ -1,14 +1,20 @@
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
+    const navigate = useNavigate();
+
+    const handleLogin = () => {
+        navigate('/dashboard');
+    };
     return (
         <div>
             <h2>Login</h2>
             <input value={email} onChange ={e => setEmail(e.target.value)} placeholder="Email"></input>
             <input value={password} onChange ={e => setPassword(e.target.value)} placeholder="Password" type="password"></input>
-            <button>Log in</button>
+            <button onClick={handleLogin}>Log in</button>
         </div>
     );
 };
--- Frontend/my-react-app/src/pages/Register.js
+++ Frontend/my-react-app/src/pages/Register.js
@@ -1,14 +1,20 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Register = () => {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
+    const navigate = useNavigate();
+
+    const handleRegister = () => {
+        navigate('/dashboard');
+    }
     return(
         <div>
             <h2>Register</h2>
             <input value={email} placeholder="Email" onChange={e => setEmail(e.target.value)}></input>
             <input value={password} placeholder="Password" onChange={e => setPassword(e.target.value)} type="password"></input>
-            <button>Register</button>
+            <button onClick={handleRegister}>Register</button>
         </div>
     );
 };
